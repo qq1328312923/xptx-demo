a736cefffd5c4a87422b083a37a69e8f81571be2
xuyw <1328312923@qq.com>
2019-09-23 22:12:07 +0800

    测试提交

---
+++ src/main/java/com/xyw/code/xptx/XptxApplication.java
@@ -0,0 +1,13 @@
+package com.xyw.code.xptx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XptxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XptxApplication.class, args);
+    }
+
+}
