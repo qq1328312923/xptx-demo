2505c11b26230a578eea373d3fe8564e48856083
xuyw <1328312923@qq.com>
2019-10-12 15:03:51 +0800

    pom文件提交

---
+++ xptx-project/xptx-system-base/xptx-system-base-server/src/main/java/com/xyw/code/base/server/XptxSystemBaseServerApplication.java
@@ -0,0 +1,13 @@
+package com.xyw.code.base.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XptxSystemBaseServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XptxSystemBaseServerApplication.class, args);
+    }
+
+}
