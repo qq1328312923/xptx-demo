054b5733560e95b681fb4c368ed5fd3bbef23de6
xuyw <1328312923@qq.com>
2019-10-21 15:07:32 +0800

    启动主类push

---
+++ xptx-gateway/src/main/java/com/xyw/code/gateway/XptxGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.xyw.code.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XptxGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XptxGatewayApplication.class, args);
+    }
+
+}
